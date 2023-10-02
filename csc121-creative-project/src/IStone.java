8355cc882c3259c4942c7e77b9b34da3cbf90c3e
ethanperl <ethan.perlakowski@vikings.berry.edu>
2023-10-01 20:48:47 -0400

    Player Classes
    Implemented two player classes and started on some methods to keep track of the stats and  standing of each player.

--- csc121-creative-project/src/IStone.java
+++ csc121-creative-project/src/IStone.java
@@ -5,26 +5,84 @@ public interface IStone {
 }
 
 class WhiteStone implements IStone{
-    int row;
-    int col;
-    
-    public WhiteStone(int row, int col) {
-        this.row = row;
-        this.col = col;
-    }
+	int row;
+	int col;
 
-    public PApplet draw(PApplet c) {
-        c.fill(255);
-        c.circle(col * 60, row * 60, 60);
-        return c;
-    }
-	
+	public WhiteStone(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+
+	public PApplet draw(PApplet c) {
+		c.fill(255);
+		c.circle(col * 60, row * 60, 60);
+		return c;
+	}
+
 }
 
 class BlackStone implements IStone{
-	IStone black;
+	int row;
+	int col;
 
-	public BlackStone(IStone black) {
+	public BlackStone(int row, int col) {
+		this.row = row;
+		this.col = col;
+	}
+
+	public PApplet draw(PApplet c) {
+		c.fill(0);
+		c.circle(col * 60, row * 60, 60);
+		return c;
+	}
+
+}
+
+
+class Player1 {
+	IStone white;
+	int cur;
+	int capStones;
+
+	public Player1(IStone white, int cur, int capStones) {
+		this.white = white;
+		this.cur = cur;
+		this.capStones = capStones;
+	}
+
+	public boolean curPlayer() {
+		return this.cur == 1;
+	}
+
+	public int getCapturedStones() {
+		return capStones;
+	}
+	
+	public void incrementCapturedStones(int count) {
+		capStones += count;
+	}
+}
+
+class Player2 {
+	IStone black;
+	int cur;
+	int capStones;
+
+	public Player2(IStone black, int cur, int capStones) {
 		this.black = black;
+		this.cur = cur;
+		this.capStones = capStones;
+	}
+
+	public boolean curPlayer() {
+		return this.cur == 2;
+	}
+
+	public int getCapturedStones() {
+		return capStones;
+	}
+
+	public void incrementCapturedStones(int count) {
+		capStones += count;
 	}
 }
