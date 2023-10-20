d16831037d358e9d9315f4f2f991389160e15b4b
ethanperl <ethan.perlakowski@vikings.berry.edu>
2023-10-20 09:35:40 -0400

    More cosmetics
    Cleaned up spacing and commented out code. Removing unused/redundant code.

--- csc121-creative-project/src/GoWorld.java
+++ csc121-creative-project/src/GoWorld.java
@@ -7,100 +7,55 @@ import processing.event.MouseEvent;
 
 public class GoWorld {
 	Board board;
-	//Player[] player;
-	// int curP = 0 / 1;
-    
 
-    
-    public static int GRID_SIZE = 80;
-    public static int GRID_MARGIN = 30;
-    public static int STONE_SIZE = 45;
+	public static int GRID_SIZE = 80;
+	public static int GRID_MARGIN = 30;
+	public static int STONE_SIZE = 45;
 
-    
-   
-    public GoWorld(Board board) {
+	public GoWorld(Board board) {
 		this.board = board;
-	//	this.player[0] = new Player( Intersection.WHITE, .board.cols..)
-		//this.player = Intersection.WHITE;
 	}
 
-    /*
-     * calls the draw method of the boards 
-     */
+	/*
+	 * calls the draw method of the boards 
+	 */
 	public PApplet draw(PApplet c) { 
 		return board.draw(c);
-    }
+	}
 
-    /*
-     * Draws a circle of a specified diameter at a given (x,y) position
-     */
-    public void drawCircle(PApplet c, float x, float y) {
-    	int diameter = 60;
-        c.circle(x, y, diameter);
-    }
-    
-    /*
-     * TODO 
-     * Place a circle of correct color on the nearest intersection to the (x,y) location of the mouse click
-     * Should handle the board actions when a the mouse is pressed 
-     */
-    
-    /*
-     * given the y-position of a supposed mouse click, return which row the stone should lie on
-     * 
-     */
-    public static int logicalRow(int yPos) {
-    	return (yPos - GRID_MARGIN + (GRID_SIZE/2)) / GRID_SIZE;
-    	/*
-    	int rowSpace = 30;
-    	int rows = 9; 
-    	int row = -1; // talk to Dr. Hamid about this 
-    	for(int i = 0; i <= rows; i++) {
-    		if(((i * rowSpace + 15) > yPos) && ((i * rowSpace) < yPos)) {
-    			row = i;  
-    			break;
-    		}
-    	}
-    	return row;  
-    	*/
-    }
-    
-    public static int logicalCol(int xPos) {
-    	return (xPos - 30 /* align to 0 */ + 40 /* center on the grid line */) / 80;
-    	/*int colSpace = 30;
-    	int cols = 9; 
-    	int col = -1; // talk to Dr. Hamid about this 
-    	for(int i = 0; i <= cols; i++) {
-    		if(((i * colSpace + 15) > xPos) && ((i * colSpace) < xPos)) {
-    			col = i;  
-    			break;
-    		}
-    	}
-    	return col; 
-    	*/ 
-    }
-    
-    public static int physicalX(int col) {
-    	return (col * GRID_SIZE) + GRID_MARGIN;
-    }
-    
-    public GoWorld mousePressed(MouseEvent mev) {
-    	int logCol = this.logicalCol(mev.getX()); 
-        int logRow = this.logicalRow(mev.getY());
-        
-        
-        if (this.board.get(logRow, logCol) == Intersection.EMPTY) {
-        	board.set(logRow, logCol, this.board.getColor());  
-      
-        	this.board.rotatePlayer();  // TODO: get rid of this
-        }
-        
-        // int n = board.detectCaptures(player[curP]);  // detect number of captures by the current player
-        // score += n;
-        // curP = 1 - curP;   // flips between 0 and 1
-    	
-        return this;
-    }
+	/*
+	 * Draws a circle of a specified diameter at a given (x,y) position
+	 */
+	public void drawCircle(PApplet c, float x, float y) {
+		int diameter = 60;
+		c.circle(x, y, diameter);
+	}
 
+	/*
+	 * given the y-position of a supposed mouse click, return which row the stone should lie on
+	 */
+	public static int logicalRow(int yPos) {
+		return (yPos - GRID_MARGIN + (GRID_SIZE/2)) / GRID_SIZE;
+	}
 
+	public static int logicalCol(int xPos) {
+		return (xPos - 30 /* align to 0 */ + 40 /* center on the grid line */) / 80;
+	}
+
+	public static int physicalX(int col) {
+		return (col * GRID_SIZE) + GRID_MARGIN;
+	}
+
+	public GoWorld mousePressed(MouseEvent mev) {
+		int logCol = this.logicalCol(mev.getX()); 
+		int logRow = this.logicalRow(mev.getY());
+
+		if (this.board.get(logRow, logCol) == Intersection.EMPTY) {
+			board.set(logRow, logCol, this.board.getColor());  
+
+			this.board.rotatePlayer();  // TODO: get rid of this
+		}
+		
+		return this;
+	}
 }
--- csc121-creative-project/src/IBoard.java
+++ csc121-creative-project/src/IBoard.java
@@ -1,28 +1,23 @@
-import java.awt.Color;
-
 import processing.core.PApplet;
 import processing.core.PVector;
 
-
- enum Intersection {
+enum Intersection {
 	EMPTY, WHITE, BLACK;
-	
+
 	public PApplet draw(PApplet c, int x, int y) {
 		if (this != EMPTY) {
 			if (this == WHITE) {
 				c.stroke(225);
 				c.fill(255);
-				
-				
+
 			} else if (this == BLACK) {
 				c.stroke(0);
 				c.fill(0);
-				
-				
 			}
-			
+
 			c.circle(x, y, GoWorld.STONE_SIZE );
 		}
+
 		return c;
 	}
 }
@@ -45,7 +40,7 @@ class Board {
 			this.pts[i] = Intersection.EMPTY;
 		}
 	}
-	
+
 	/* return the piece at the given row and col */
 	public Intersection get(int row, int col) {
 		return pts[(row * this.cols) + col];
@@ -54,15 +49,14 @@ class Board {
 	/*
 	 *  places a piece at row, column in the points list 
 	 */
-	
+
 	public void set(int row, int col, Intersection pc) {
 		pts[(row * this.cols) + col] = pc; 
 	}
-	
+
 	/*
 	 * given a row and col, return the positions respective location in the pts[] array
 	 */
-	
 	public int getLoc(int row, int col) {
 		return (row * this.cols) + col; 
 	}
@@ -71,61 +65,60 @@ class Board {
 	 * draws a 9x9 board
 	 */
 	public PApplet draw(PApplet c) {
-        int borderWidth;
-        int borderHeight; 
-        int sideLength;
-        
-        c.background(220, 196, 149); 
-        
-        borderWidth = 30;
-        borderHeight = 30; 
-        sideLength = 80;
-        while(borderWidth < 700) {
-        	c.stroke(0);
-            c.line(borderWidth, borderHeight, borderWidth, 670); 
-            borderWidth = borderWidth + sideLength; 
-        }
-        borderWidth = GoWorld.GRID_MARGIN;
-        borderHeight = GoWorld.GRID_MARGIN; 
-        sideLength = GoWorld.GRID_SIZE;
-        while(borderHeight < 700) {
-        	c.stroke(0);
-            c.line(borderWidth, borderHeight, 670, borderHeight); 
-            borderHeight = borderHeight + sideLength; 
-        }
-        
-        for (int row = 0; row <= 8; row++) {
-        	for (int col = 0; col <= 8; col++) {
-        		int x = GoWorld.physicalX(col);
-        		int y = GoWorld.physicalX(row);
-        		get(row, col).draw(c, x, y);
-        	}
-        }
-        
-        return c;
-    }
-	
-	
+		int borderWidth;
+		int borderHeight; 
+		int sideLength;
+
+		c.background(220, 196, 149); 
+
+		borderWidth = 30;
+		borderHeight = 30; 
+		sideLength = 80;
+		while(borderWidth < 700) {
+			c.stroke(0);
+			c.line(borderWidth, borderHeight, borderWidth, 670); 
+			borderWidth = borderWidth + sideLength; 
+		}
+
+		borderWidth = GoWorld.GRID_MARGIN;
+		borderHeight = GoWorld.GRID_MARGIN; 
+		sideLength = GoWorld.GRID_SIZE;
+		while(borderHeight < 700) {
+			c.stroke(0);
+			c.line(borderWidth, borderHeight, 670, borderHeight); 
+			borderHeight = borderHeight + sideLength; 
+		}
+
+		for (int row = 0; row <= 8; row++) {
+			for (int col = 0; col <= 8; col++) {
+				int x = GoWorld.physicalX(col);
+				int y = GoWorld.physicalX(row);
+				get(row, col).draw(c, x, y);
+			}
+		}
+
+		return c;
+	}
+
 	public Intersection getColor() {
 		if(this.player == 0) {
 			return Intersection.BLACK; 
 		}
+
 		else if(this.player == 1) {
 			return Intersection.WHITE; 
 		}
+
 		return Intersection.EMPTY;  // if player is either 1 or 0 then when we want to return empty it never will
 	}
-	
+
+	/*
+	 * Switches between player 1 and 2
+	 */
 	public void rotatePlayer() {
 		this.player = 1 - this.player; 
 	}
-	
 
-	public int getIntersectionSize() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-	
 	/*
 	 * given a location for a piece in the pts[] array, a vertical and horizontal direction to check
 	 * and a piece color, determine if the piece is touching a piece of opposite color in the given direction
@@ -135,7 +128,7 @@ class Board {
 	 */
 	public boolean checkSurrInDir(int loc, int vDir, int hDir, Intersection color) {
 		int listPos = loc + (vDir * this.cols) + hDir; 
-		
+
 		if (this.offBoard(loc, hDir, vDir)){
 			return true; 
 		}
@@ -146,11 +139,11 @@ class Board {
 			return false;
 		}
 		else {
+
 			return true;
 		}
-
 	}
-	
+
 	/*
 	 * HELPER METHOD FOR checkSurrInDir
 	 * Because we represent our board as a 1-d array of points, it is hard to 
@@ -165,25 +158,27 @@ class Board {
 			return true;
 		}
 		else if( (hDir < 0) && (loc % this.cols == 0) ) { 
+
 			// case where loc is in first column and trying to move horizontally to the left
 			return true;
 		}
 		else if( (hDir > 0) && (loc % this.cols == 8) ) { 
+
 			// case where loc is in the last column and trying to move horizontally to the right
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	public boolean checkSurr(int loc, Intersection color) {
 		return 
 				this.checkSurrInDir(loc, 1, 0, color) && 
 				this.checkSurrInDir(loc, -1, 0, color) &&
 				this.checkSurrInDir(loc, 0, 1, color) &&
 				this.checkSurrInDir(loc, 0, -1, color); 
-					
-		 
+
+
 	}
 
 }
